678021ec78faf36a87478eb8cc257f3b89642dc2
AndreyZabuyskiy <zabuyskiy84@gmail.com>
2022-06-11 10:59:39 +0300

    Added projects

---
+++ LinkShortener/LinkShortener.LinkShortenerApi/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LinkShortener.LinkShortenerApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : Controller
+{
+    [HttpGet]
+    public IActionResult Hello()
+    {
+        return Ok("Success");
+    }
+}
