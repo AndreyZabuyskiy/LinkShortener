7e2f5daea369067743d4d4750e63447cf6efd29e
AndreyZabuyskiy <zabuyskiy84@gmail.com>
2022-06-11 11:10:51 +0300

    Added models

---
+++ LinkShortener/Domain/Url.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain;
+
+public class Url
+{
+    public Guid Id { get; set; }
+    public string FullUrl { get; set; }
+    public string ShortUrl { get; set; }
+    public User User { get; set; }
+}
