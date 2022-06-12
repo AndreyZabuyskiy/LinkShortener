948dbad854243dd35d030de54dc0050d6251da1b
AndreyZabuyskiy <zabuyskiy84@gmail.com>
2022-06-12 15:44:34 +0300

    Added register form

--- reactclient/src/components/RegisterForm.tsx
+++ reactclient/src/components/RegisterForm.tsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
+
 const RegisterForm = () => {
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+
+  
+
   return (
-    <div>Register form</div>
+    <form>
+      <h1 className="h3 mb-3 fw-normal">Please register</h1>
+      <input type="text" className="form-control" placeholder="login" />
+      <input type="password" className="form-control" placeholder="Password" />
+      <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+    </form>
   );
 }
 
